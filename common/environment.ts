145b5eb1b12dbac49251a8fea2c8b01d9ccd6e96
Thiago Henrique dos Santos <henrique.thsantos.ths@gmail.com>
2020-10-29 08:48:27 -0300

    Realizada a conexão com o mongoDB
    Conexão realizada com sucesso usando mongoose

--- common/environment.ts
+++ common/environment.ts
@@ -2,4 +2,7 @@ export const environment = {
     server: {
         port: process.env.SERVER_PORT || 3333,
     },
+    db: {
+        url: process.env.DB_URL || 'mongodb://localhost/meat-api',
+    },
 };
